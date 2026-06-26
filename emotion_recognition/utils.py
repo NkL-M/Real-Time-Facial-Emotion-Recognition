5179609fb3d7ef1e81655dd397e5412db9f118da
Nicolas Marechal <marechal.n@hotmail.com>
2026-06-26 03:45:23 +0200

    feat: function for evaluating model inference time added

--- emotion_recognition/utils.py
+++ emotion_recognition/utils.py
@@ -1,13 +1,17 @@
 """ Module for various functions for plotting. """
 
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
-from keras import Model
+import onnxruntime as ort
+from keras import Model, models
+from pathlib import Path
 from sklearn.metrics import classification_report, confusion_matrix
 
 from emotion_recognition.src.data import load_data
+from emotion_recognition.src.registry import load_tf_model
 from emotion_recognition.params import EMOTIONS_CLASSES
 
 
@@ -114,3 +118,70 @@ def conf_matrix(model: Model, normalize: bool = True) -> None :
     plt.yticks(rotation=0)
     plt.tight_layout()
     plt.show()
+
+
+def tf_model_avg_inference_time(model_path: Path,
+                                model_format: str = 'TF'):
+    """
+    Measures and prints the average inference time of a model (TensorFlow or ONNX).
+
+    This function loads a pre-trained model in the specified format, performs a warmup phase
+    to stabilize performance, and then measures the average inference time over 100 runs
+    using a dummy input of shape (1, 48, 48, 1).
+
+    Args
+    ----
+    model_path : Path)
+        - Path to the model file.
+
+    model_format : str (optional)
+        - Format of the model, either 'TF' (TensorFlow) or 'ONNX'. Defaults to 'TF'.
+
+    Returns:
+        None: Prints the average inference time in milliseconds to the console.
+
+    Raises:
+        ValueError: If an unsupported `model_format` is provided.
+
+    Notes:
+        - The warmup phase consists of 10 inference runs to avoid cold-start bias.
+        - The dummy input is a random array of shape (1, 48, 48, 1) with dtype float32.
+        - For ONNX models, the `CPUExecutionProvider` is used for inference.
+    """
+    model_format = model_format.upper()
+    formats = ['TF', 'ONNX']
+
+    if model_format in formats:
+        if model_format=='TF':
+            model = models.load_model(model_path)
+
+        else:
+            session = ort.InferenceSession(model_path, providers=["CPUExecutionProvider"]) # Load ONNX model
+            input_name = session.get_inputs()[0].name
+            output_name = session.get_outputs()[0].name
+
+        dummy = np.random.rand(1, 48, 48, 1).astype("float32") # Dummy input
+
+        # Warmup
+        for i in range(10):
+            if model_format=='TF':
+                model.predict(dummy)
+            else:
+                session.run([output_name], {input_name: dummy})
+
+        # Timed inference
+        n = 100
+        start = time.time()
+        for i in range(n):
+            if model_format=='TF':
+                model.predict(dummy)
+            else:
+                session.run([output_name], {input_name: dummy})
+        end = time.time()
+
+        avg_inference_time_ms = (end - start) / n * 1000
+
+        print(f'The {model_format} model average inference time: {round(avg_inference_time_ms, 2)} ms')
+
+    else:
+        raise ValueError(f"Wrong model format '{model_format}'")
